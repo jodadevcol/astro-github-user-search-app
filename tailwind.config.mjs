09d7ee452b59c0b41b78d9cf016fb695481c028b
Danni Garcia <jodadev.col@gmail.com>
2024-09-21 10:58:57 +1000

    Update tsconfig.json and astro.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,65 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: ['Space Mono', 'monospace'],
+			},
+			colors: {
+				shared: {
+					100: '#0079FF',
+				},
+				light: {
+					100: '#697C9A',
+					200: '#4B6A9B',
+					300: '#2B3442',
+					400: '#F6F8FF',
+					500: '#FEFEFE',
+				},
+				dark: {
+					100: '#FFFFFF',
+					200: '#141D2F',
+					300: '#1E2A47',
+				}
+			},
+			fontSize:{
+				h1: [
+					"1.625rem",
+					{
+						lineHeight: "2.375rem",
+						fontWeight: "700",
+					},
+				],
+				h2: [
+					"1.375rem",
+					{
+						lineHeight: "2.063rem",
+						fontWeight: "700",
+					},
+				],
+				h3: [
+					"1rem",
+					{
+						lineHeight: "1.5rem",
+						fontWeight: "400",
+					},
+				],
+				h4: [
+					"0.813rem",
+					{
+						lineHeight: "1.25rem",
+						fontWeight: "400",
+					},
+				],
+				body: [
+					"0.938rem",
+					{
+						lineHeight: "1.563rem",
+						fontWeight: "400",
+					},
+				],
+			}
+		},
 	},
 	plugins: [],
 }
